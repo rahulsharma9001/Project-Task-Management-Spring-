f17e0633497a9f46e81daa89fcfc10ccdb2fbddb
Rahul Sharma <rahul.sharmaa9001@gmail.com>
2025-03-26 01:33:09 +0530

    Added Password Encoder

--- src/main/java/com/rahulsharma/Project_Task_Management/config/SecurityConfig.java
+++ src/main/java/com/rahulsharma/Project_Task_Management/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.rahulsharma.Project_Task_Management.config;
 
+import com.rahulsharma.Project_Task_Management.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +25,16 @@ public class SecurityConfig {
         httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("rahul").password(passwordEncoder().encode("1234")).authorities("USER").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
